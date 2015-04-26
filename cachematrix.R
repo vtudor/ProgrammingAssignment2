8a6c93f73bac6b4a21afe3084ae45bf20d9b57c3
vtudor <vtudor@gmail.com>
2015-04-26 17:50:40 +0300

    implemented makeCacheMatrix function

--- cachematrix.R
+++ cachematrix.R
@@ -3,8 +3,27 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## The function makeVector creates a special "vector", which is really a list containing a function to
+##    set the value of the vector
+##    get the value of the vector
+##    set the value of the mean
+##    get the value of the mean
 
+makeCacheMatrix <- function(x = matrix()) {
+		invX <- NULL			## init the inverse of x
+        
+		set <- function(y) {	## set the matrix
+                x <<- y
+                invX <<- NULL
+        }
+        get <- function() x		## get the matrix
+        
+		setInverse <- function(inverse) invX <<- inverse		## set the inverse matrix
+        getInverse <- function() invX							## get the inverse matrix
+        
+		list(set = set, get = get,			## return
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
